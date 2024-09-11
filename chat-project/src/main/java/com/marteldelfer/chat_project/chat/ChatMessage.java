acc4efe8b266cc8a1feea4fd551a687e4d95df17
Murillo Ferraz <marteldelfer@gmail.com>
2024-09-11 02:37:19 -0300

    Create WebSocketConfig and FrontEnd

---
+++ chat-project/src/main/java/com/marteldelfer/chat_project/chat/ChatMessage.java
@@ -0,0 +1,21 @@
+package com.marteldelfer.chat_project.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+    
+    private String content;
+    private String sender;
+    private MessageType type;
+
+}
---
+++ chat-project/src/main/java/com/marteldelfer/chat_project/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.marteldelfer.chat_project.chat;
+
+public enum MessageType {
+    
+    CHAT,
+    JOIN,
+    LEAVE
+}
